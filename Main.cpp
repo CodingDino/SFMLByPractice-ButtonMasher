f54113a9681c3dfa74290d7275fa750628cef6f9
Sarah Herzog <sarah.ann.herzog@gmail.com>
2018-06-27 03:53:02 +0100

    Added Hello World logging to main function

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,8 @@
-int main()
-{
-	return 0;
-}
+#include <iostream>								// Include the library we need to print stuff
+
+int main()										// The start of our main() function, starting point of the program
+{												// curly brackets that contain the main() function.
+	std::cout << "Hello World!" << std::endl;	// Prints text to console
+	system("PAUSE");							// Makes the computer wait before going on to next statement
+	return 0;									// Exit point for the program - closes the console window
+}												// curly brackets that contain the main() function.
