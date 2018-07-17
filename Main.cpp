d6f83ed6e103687bc7c54de39e40d2c812956853
Sarah Herzog <sarah.ann.herzog@gmail.com>
2018-07-17 08:58:36 +0100

    SFML Window Setup

--- Main.cpp
+++ Main.cpp
@@ -1,23 +1,58 @@
-#include <iostream>
+// Library Includes
+// Library needed for creating and managing SFML windows
+#include <SFML/Window.hpp>	
+// Library needed for using sprites and textures
+#include <SFML/Graphics.hpp>
 
+// The main() Function - entry point for our program
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	// Declare our SFML window, called gameWindow
+	sf::Window gameWindow;
+	// Set up the SFML window, passing the dimmensions and the window name
+	gameWindow.create(sf::VideoMode::getDesktopMode(), "Button Masher", sf::Style::Titlebar | sf::Style::Close);
 
-	int loopCounter = 0;
+	// -----------------------------------------------
+	// Game Setup
+	// -----------------------------------------------
+	sf::Texture buttonTexture;
+	buttonTexture.loadFromFile("button.png");
 
-	while (true)
+	// -----------------------------------------------
+	// Game Loop
+	// -----------------------------------------------
+	// Repeat as long as the window is open
+	while (gameWindow.isOpen())
 	{
-		loopCounter = loopCounter + 1;
+		// -----------------------------------------------
+		// Input Section
+		// -----------------------------------------------
+		// Declare a variable to hold an Event, called gameEvent
+		sf::Event gameEvent;
+		// Loop through all events and poll them, putting each one into our gameEvent variable
+		while (gameWindow.pollEvent(gameEvent))
+		{
+			// This section will be repeated for each event waiting to be processed
 
-		std::cout << "Loop number " << loopCounter << std::endl;
+			// Did the player try to close the window?
+			if (gameEvent.type == sf::Event::Closed)
+			{
+				// If so, call the close function on the window.
+				gameWindow.close();
+			}
+		} // End event polling loop
 
-		// TODO: Check for input
-
+		// -----------------------------------------------
+		// Update Section
+		// -----------------------------------------------
 		// TODO: Update game state
 
+		// -----------------------------------------------
+		// Draw Section
+		// -----------------------------------------------
 		// TODO: Draw graphics
 
-	}
+	} // End of Game Loop
 	return 0;
-}
+
+} // End of main() Function
